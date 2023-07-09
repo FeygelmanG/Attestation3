e097e66630f153b067c28ae9ac48f3c6495b88b4
Gulnara Feygelman <gulya_b@yahoo.com>
2023-07-09 13:00:36 +0300

    fix yaml

--- src/test/java/ru/aptekaeconom/test/WebTest.java
+++ src/test/java/ru/aptekaeconom/test/WebTest.java
@@ -2,28 +2,37 @@ package ru.aptekaeconom.test;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URI;
-
 import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
 public class WebTest {
 
     @BeforeAll
     public static void setDriver() throws MalformedURLException {
-        boolean isRemote = true;
-        if (isRemote == true) {
-            DesiredCapabilities capabilities = new DesiredCapabilities();
-            capabilities.setBrowserName("chrome");
-            capabilities.setCapability("enableVNC:", true);
-            WebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), capabilities);
-            setWebDriver(driver);
-        } else {
+            Configuration.timeout = 6000;
             Configuration.browser = "chrome";
-        }
+
+            ChromeOptions options = new ChromeOptions();
+            RemoteWebDriver remoteWebDriver = new RemoteWebDriver(
+                    URI.create("http://localhost:4444/wd/hub").toURL(), options
+            );
+            setWebDriver(remoteWebDriver);
+
+//            Configuration.baseUrl = "https://aptekaeconom.com";
+            //Configuration.pageLoadStrategy= "eager";
+//            open("/");
+//        boolean isRemote = true;
+//        if (isRemote == true) {
+//            DesiredCapabilities capabilities = new DesiredCapabilities();
+//            capabilities.setBrowserName("chrome");
+//            capabilities.setCapability("enableVNC:", true);
+//            WebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), capabilities);
+//            setWebDriver(driver);
+//        } else {
+//            Configuration.browser = "chrome";
+//        }
     }
 }
