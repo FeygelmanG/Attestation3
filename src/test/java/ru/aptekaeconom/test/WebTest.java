7cbcde8e3cc9cab36345916756b284748b7f1603
Gulnara Feygelman <gulya_b@yahoo.com>
2023-07-13 19:47:04 +0300

    fix yaml

--- src/test/java/ru/aptekaeconom/test/WebTest.java
+++ src/test/java/ru/aptekaeconom/test/WebTest.java
@@ -2,13 +2,10 @@ package ru.aptekaeconom.test;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
-
 import java.net.MalformedURLException;
 import java.net.URI;
-import java.util.Objects;
 
 import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
@@ -27,28 +24,4 @@ public class WebTest {
         Configuration.browser = "chrome";
 
     }
-//        String isRemote = System.getenv("IS_REMOTE");
-//        if (Objects.equals(isRemote, "true")) {
-//            ChromeOptions chromeOptions = new ChromeOptions();
-//            chromeOptions.setCapability("enableVNC:", true);
-//            WebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), chromeOptions);
-//            setWebDriver(driver);
-//        } else {
-//            Configuration.browser = "chrome";
-//        }
-//    }
-//        boolean isRemote = true;
-//        if (isRemote == true) {
-//            Configuration.timeout = 6000;
-//            Configuration.browser = "chrome";
-//
-//            ChromeOptions options = new ChromeOptions();
-//            RemoteWebDriver remoteWebDriver = new RemoteWebDriver(
-//                    URI.create("http://localhost:4444/wd/hub").toURL(), options
-//            );
-//            setWebDriver(remoteWebDriver);
-//        } else {
-//            Configuration.browser = "chrome";
-//        }
-    //}
 }
