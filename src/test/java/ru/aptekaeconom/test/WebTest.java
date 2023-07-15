1f89237d2e7360503f8ac17a32ea698f28e14197
Gulnara Feygelman <gulya_b@yahoo.com>
2023-07-15 20:07:17 +0300

    fix yaml

--- src/test/java/ru/aptekaeconom/test/WebTest.java
+++ src/test/java/ru/aptekaeconom/test/WebTest.java
@@ -2,10 +2,13 @@ package ru.aptekaeconom.test;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
 import java.net.MalformedURLException;
 import java.net.URI;
+import java.util.Objects;
 
 import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
 
@@ -13,15 +16,14 @@ public class WebTest {
 
     @BeforeAll
     public static void setDriver() throws MalformedURLException {
-        Configuration.timeout = 6000;
-        Configuration.browser = "chrome";
-
-        ChromeOptions options = new ChromeOptions();
-        RemoteWebDriver remoteWebDriver = new RemoteWebDriver(
-                URI.create("http://localhost:4444/wd/hub").toURL(), options
-        );
-        setWebDriver(remoteWebDriver);
-        //onfiguration.browser = "chrome";
-
+        String isRemote = System.getenv("IS_REMOTE");
+        if (Objects.equals(isRemote, "true")) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setCapability("enableVNC:", true);
+            WebDriver driver = new RemoteWebDriver(URI.create("http://localhost:4444/wd/hub").toURL(), chromeOptions);
+            setWebDriver(driver);
+        } else {
+            Configuration.browser = "chrome";
+        }
     }
 }
